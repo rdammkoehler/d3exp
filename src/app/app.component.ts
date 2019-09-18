fdb5d40bc57c862a2ed067d0097e381720c33405
Rich Dammkoehler <rpd@noradltd.com>
2019-09-18 10:57:25 -0500

    blah

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ interface BallGraphConfiguration {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent implements AfterContentInit {
